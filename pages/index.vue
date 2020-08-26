a550bcbc85d65d29c9d162de63d2697870660493
Sam <s.sam25@laposte.net>
2020-08-26 16:18:12 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-flex xs12 sm8 md6>
       <v-btn @click="generate">Generate</v-btn>
 
-      <n-link tag="li" :to="datenow" v-if="clicked">
+      <n-link :to="datenow" v-if="clicked">
         <a>myawesomeadress/{{datenow}}</a>
       </n-link>
     </v-flex>
